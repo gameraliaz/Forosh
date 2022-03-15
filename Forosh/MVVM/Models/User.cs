88db6bbd52480aaf0624991d52f2b5a62352bddc
gameraliaz <gameraliaz@gmail.com>
2022-03-15 16:40:17 +0330

    main window style M

---
+++ Forosh/MVVM/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forosh.MVVM.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string FullName 
+        { 
+            get
+            {
+                return FirstName+ " " + LastName;
+            }
+        }
+        public DateTime LoginTime { get; set; }
+    }
+}
